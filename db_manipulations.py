b0761cc9f08faf4afe601daa710df3c961284a78
or <ormull@post.bgu.ac.il>
2024-06-01 14:37:50 +0300

    first commit for text in data

--- db_manipulations.py
+++ db_manipulations.py
@@ -1,2 +1,8 @@
 import sqlite3
+from searches import search_posts
 conn = sqlite3.connect('data_for_text.db')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS peptides (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL)''')
+peptides = []
--- searches.py
+++ searches.py
@@ -5,14 +5,30 @@ import praw
 import pandas as pd
 import numpy as py
 import reddit_auth_details
-from main import  nootropics,peptides,bio_hacks
+from main import nootropics, peptides, bio_hacks
 import praw
-posts = {}
+from collections import namedtuple
+from collections import Counter
 
-def search_posts(subreddit,query,sort="relevance"):
-    resaults = subreddit.search(query,limit = 100,sort)
+
+
+key = namedtuple('key', ['query', 'title', 'comments', 'upvotes', 'author'])
+
+
+def check_submission(sub1, posts):
+    if sub1.id in posts.keys(): return
+
+
+def search_posts(subreddit, query, subject,posts, sorting="relevance"):
+    resaults = subreddit.search(query, limit=100, sort=sorting)
+    if subject not in posts:
+        posts[subject] = []
     for res in resaults:
-        posts[res.id]=res
+        posts[subject].append(())
         print(res.title)
 
 
+def get_comments(posts, key):
+    comments = {}
+posts={}
+search_posts(peptides, "bpc", "bpc-157",posts)
