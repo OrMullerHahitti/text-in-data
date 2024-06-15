3d213c1ae4d02dd06b56ee3d7aff6078033432bb
or <ormull@post.bgu.ac.il>
2024-06-15 15:02:05 +0300

    a new update run only the code in main code for sna

--- collect_searches.py
+++ collect_searches.py
@@ -12,6 +12,16 @@ posts = load_posts("data_collected/posts.pkl")
 
 for peptide in peptides:
     search_posts(reddit, "peptides", peptide, peptide, posts, limit=2)
+for nootropic in nootropics:
+    search_posts(reddit, "nootropics", nootropic, nootropic, posts, limit=2)
+for genre in genres:
+    search_posts(reddit, "peptides", genre, genre, posts,limit = 2)
+for genre in genres:
+    search_posts(reddit, "nootropics", genre, genre, posts,limit = 3)
+for genre in genres:
+    search_posts(reddit, "bio-hacks", genre, genre, posts,limit = 3)
+
+
 
 save_posts(posts, "data_collected/posts.pkl")
 
